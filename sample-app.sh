6b04f75a12270de933b8b15052dac7233367cfd7
Stevens Bram <47774155+BramStevens1998@users.noreply.github.com>
2021-12-20 16:04:46 +0100

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -9,7 +9,7 @@ cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
-cat > tempdir/Dockerfile << _EOF_
+cat > tempdir/Dockerfile << EOF
 FROM python
 RUN pip install flask
 COPY  ./static /home/myapp/static/
@@ -17,9 +17,9 @@ COPY  ./templates /home/myapp/templates/
 COPY  sample_app.py /home/myapp/
 EXPOSE 5050
 CMD python /home/myapp/sample_app.py
-_EOF_
+EOF
 
 cd tempdir || exit
 docker build -t sampleapp .
 docker run -t -d -p 5050:5050 --name samplerunning sampleapp
-docker ps -a 
+docker ps -a
